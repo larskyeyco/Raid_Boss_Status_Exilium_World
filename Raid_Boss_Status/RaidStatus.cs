dfd9654ec70569d5fb85028b3b49c87b463577b0
d0nat <szy.szymalski@gmail.com>
2022-01-01 14:28:03 +0100

    Load and parse HTML

---
+++ Raid_Boss_Status/RaidStatus.cs
@@ -0,0 +1,16 @@
+﻿using HtmlAgilityPack;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Raid_Boss_Status
+{
+    public class RaidStatus
+    {
+        public static void RaidBossList()
+        {
+            var raidBossList = new List<RaidBossModel>();
+            HtmlWeb web = new HtmlWeb();
+            HtmlDocument document = web.Load("https://www.exiliumworld.com/servers/faris/boss");
+        }
+    }
+}
