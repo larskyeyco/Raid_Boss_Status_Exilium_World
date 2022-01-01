2b17f1fdfd36eed359ec3792eff2939b40b80aaa
d0nat <szy.szymalski@gmail.com>
2022-01-01 14:23:42 +0100

    Add RaidBossModel

---
+++ Raid_Boss_Status/RaidBossModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Raid_Boss_Status
+{
+    public class RaidBossModel
+    {
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string Status { get; set; }
+        public string Spawn_Date { get; set; }
+    }
+}
